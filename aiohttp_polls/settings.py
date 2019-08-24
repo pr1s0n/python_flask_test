ccff156143e4761dbdc04661357891c83be189ad
pr1s0n <li1111111@vip.qq.com>
2019-08-24 22:53:03 +0800

    更改配置目录名

--- aiohttp_polls/settings.py
+++ aiohttp_polls/settings.py
@@ -2,7 +2,7 @@ import pathlib
 import yaml
 
 BASE_DAR = pathlib.Path(__file__).parent.parent
-config_path = BASE_DAR / 'conf' / 'polls.yaml'
+config_path = BASE_DAR / 'config' / 'polls.yaml'
 def get_config(path):
 	with open(path) as f:
 		config = yaml.safe_load(f)
