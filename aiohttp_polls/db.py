6c5e22f0455ddb81d45732ca465f5a0c4955587c
pr1s0n <li1111111@vip.qq.com>
2019-08-24 22:52:35 +0800

    db修改integer

--- aiohttp_polls/db.py
+++ aiohttp_polls/db.py
@@ -1,11 +1,11 @@
 from sqlalchemy import (
-	MetaData, Table, Column, ForeignKey, Interger, String, Date
+	MetaData, Table, Column, ForeignKey, Integer, String, Date
 )
 
 meta = MetaData()
 question = Table(
 	'question', meta,
-	Column('id',Interger,primary_key=True),
+	Column('id',Integer,primary_key=True),
 	Column('question_text',String(200),nullable=False),
 	Column('pub_date',Date,nullable=False)
 
@@ -13,10 +13,10 @@ question = Table(
 choice = Table(
 
 	'choice',meta,
-	Column('id',Interger,primary_key=True),
+	Column('id',Integer,primary_key=True),
 	Column('choice_text',String(200),nullable=False),
-	Column('votes',Interger,server_default='0',nullable=False),
-	Column('question',Interger,
+	Column('votes',Integer,server_default='0',nullable=False),
+	Column('question',Integer,
 		ForeignKey(
 
 			'question.id',ondelete='CASCADE'
