7de2b1a3bbb603d241857825b487419f33fafa2a
pr1s0n <li1111111@vip.qq.com>
2019-08-25 22:20:20 +0800

    使用模板引擎实现数据展示

--- aiohttp_polls/views.py
+++ aiohttp_polls/views.py
@@ -1,4 +1,11 @@
-from aiohttp import web
+# aiohttpdemo_polls/views.py
+import aiohttp_jinja2
+import db
+
+@aiohttp_jinja2.template('index.html')
 async def index(request):
-	return web.Response(text='Hello Aiohttp')
-	
+    async with request.app['db'].acquire() as conn:
+        cursor = await conn.execute(db.question.select())
+        records = await cursor.fetchall()
+        questions = [dict(q) for q in records]
+        return {"questions": questions}
