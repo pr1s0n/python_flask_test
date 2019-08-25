35978575a5e130a867a8ff8e2b322fd350cc8b91
pr1s0n <li1111111@vip.qq.com>
2019-08-25 22:19:07 +0800

    添加aiohttp jinja2模板引擎

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -1,11 +1,16 @@
 from aiohttp import web
 from routes import setup_routes
-from settings import config
+from settings import config,BASE_DIR
+from db import close_pg,init_pg
+import aiohttp_jinja2
+import jinja2
 
 app = web.Application()
-setup_routes(app)
+
 app['config'] = config
-
-
+aiohttp_jinja2.setup(app,loader = jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttp_polls' / 'templates')))
+setup_routes(app)
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
 
 web.run_app(app)
