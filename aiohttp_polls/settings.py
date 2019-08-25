897a8072f1e8cc234b9e9af58dc279b0e6977fc8
pr1s0n <li1111111@vip.qq.com>
2019-08-25 22:19:41 +0800

    改了目录常量名

--- aiohttp_polls/settings.py
+++ aiohttp_polls/settings.py
@@ -1,8 +1,8 @@
 import pathlib
 import yaml
 
-BASE_DAR = pathlib.Path(__file__).parent.parent
-config_path = BASE_DAR / 'config' / 'polls.yaml'
+BASE_DIR = pathlib.Path(__file__).parent.parent
+config_path = BASE_DIR / 'config' / 'polls.yaml'
 def get_config(path):
 	with open(path) as f:
 		config = yaml.safe_load(f)
