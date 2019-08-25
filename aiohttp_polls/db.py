15e26a9808b1bf3e87ae69a2f3b17227a161be11
pr1s0n <li1111111@vip.qq.com>
2019-08-25 22:18:27 +0800

    实现连接数据库

--- aiohttp_polls/db.py
+++ aiohttp_polls/db.py
@@ -1,7 +1,8 @@
+import aiopg.sa
 from sqlalchemy import (
 	MetaData, Table, Column, ForeignKey, Integer, String, Date
 )
-
+__all__ = ['question','choice']
 meta = MetaData()
 question = Table(
 	'question', meta,
@@ -44,3 +45,19 @@ result = await conn.execute(select([Question]))
 
 '''
 
+async def init_pg(app):
+	conf = app['config']['postgres']
+	engine = await aiopg.sa.create_engine(
+		database = conf['database'],
+		user = conf['user'],
+		password = conf['password'],
+		host = conf['host'],
+		port = conf['port'],
+		minsize = conf['minsize'],
+		maxsize = conf['maxsize'],
+	)
+	app['db'] = engine
+
+async def close_pg(app):
+	app['db'].close()
+	await app['db'].wait_closed()
