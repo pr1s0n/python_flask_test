42d8726207c6804679dcda9ab0d3e5dbdc1c75e6
pr1s0n <li1111111@vip.qq.com>
2019-08-26 21:38:25 +0800

    routes

--- aiohttp_polls/routes.py
+++ aiohttp_polls/routes.py
@@ -1,5 +1,5 @@
 from views import index
-from views import poll
+# from views import poll
 def setup_routes(app):
 	app.router.add_get('/',index)
-	app.router.add_get('/poll',poll)
+	# app.router.add_get('/poll',poll)
